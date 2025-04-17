c1fbb91df88695257d36609e722ae36055510a60
hrtz361 <0100useonly@gmail.com>
2025-04-17 10:35:44 +0700

    Add files via upload

---
+++ java/firstfileforask.java
@@ -0,0 +1,14 @@
+public class firstfileforask{
+public static void main(String[] args){
+	System.out.println("purpose of Strings ?");
+	System.out.println("purpose of args ?");
+	outsidefunction();
+	System.out.println(args[1]);
+}
+static void  outsidefunction(){
+	System.out.println("void mean function return nothing ?");
+	System.out.println("");
+	System.out.println("");
+	System.out.println("finally you need to change public class name to your file name ");
+}
+}
