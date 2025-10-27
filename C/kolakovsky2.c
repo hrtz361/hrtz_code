6ade4ef9804e367fa27fcc7a400e9620e14667cd
unknown <hrtz361@github.com>
2025-10-27 14:30:40 +0700

    modified:   kolakovsky2.c

--- C/kolakovsky2.c
+++ C/kolakovsky2.c
@@ -1,52 +1,60 @@
 #include<stdio.h>
 #include<stdlib.h>
-//pseudo code for kolakovsky string print 
+//pseudo code for kolakovsky string print
 //
 //
 int main(){
-char interger_string[10]; 
-int now=1; 
+int what=1;
+char interger_string[100];
+int now=1;
 
-for(int loop=1;loop<=10;){
+for(int loop=1;loop<=20;){
 
-	if(now==1){
-		if(atoi(&interger_string[loop]==2)){
+	if(what==1){
+		if(atoi(&interger_string[loop])==2){
 			printf("22");
 			interger_string[now]=2;
 			interger_string[now+1]=2;
-			++now;
-			++now;
+
+            now+=2;
+
+
+			what=atoi(&interger_string[now]);
+
 
 		}
-		
+
 		else{
-			print("2"); 
-			interger_string[now]=2
+			printf("2");
+			interger_string[now]=2;
 			++now;
+			what=atoi(&interger_string[now]);
 		}
 
 	}
 
 	else{
-		if(atoi(&interger_string[loop]==2)){
-		
-			printf("11"); 
+		if(atoi(&interger_string[loop])==2){
+
+			printf("11");
 			interger_string[now]=1;
 			interger_string[now]=1;
 			++now;
 			++now;
+			what=atoi(&interger_string[now]);
 		}
 		else{
-			printf("1"); 
-			interger_string[now]=1; 
-			++now; 
+			printf("1");
+			interger_string[now]=1;
+			++now;
+			what=atoi(&interger_string[now]);
 		}
 	}
 
-++loop; 
+++loop;
 }
 }
-// nexus5 editing here 
-// A13 also here 
+// nexus5 editing here
+// A13 also here
 // hey it's W10
 //
