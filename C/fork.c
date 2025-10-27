7fb26f492a3886cda6ae43eab1b9fcf0207ce1bf
* <hrtz361@github.com>
2025-10-27 14:30:53 +0700

    new file:   fork.c 	deleted:    codecode.c

---
+++ C/fork.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+
+int main(int argc,char *argv[]){
+int id = fork(); 
+int so;
+if(id!=0){
+	wait(); 
+}
+else{
+	so=getpid(); 
+}
+
+printf("*");
+while(1){} 
+
+return 0 ;
+}
