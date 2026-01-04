d3b98d9797881ade9aad525c0e14c5970a7db6ec
l2tatime <l2tatime@github.com>
2026-01-04 23:48:50 +0000

    modified:   signal.c

--- C/signal.c
+++ C/signal.c
@@ -3,13 +3,23 @@
 #include<stdlib.h>
 // what is handler is it get input by stdin ? 
 void func(int err){
-	printf("exit signal id:%d",err);
-	printf("the number?:%d",SIGINT); 
+	printf("get signal from insdec id:%d",err);
+	printf("the number?:%d\n",SIGKILL); 
+	printf("the number?:%d\n",SIGINT); 
+	printf("the number?:%d\n",SIGSEGV); 
+	printf("the number?:%d\n",SIGCHLD);
+	printf("the number?:%d\n",SIGPIPE);
+
 	exit(1); 
 }
+
+//
 int main(){
-	signal(SIGINT,func); 
+	signal(SIGQUIT,func); 
 	while(1){
 		printf("\n  * "); 
 	} 
-} 
+}
+//SIGENERATION 
+//SIGTYPES 
+//SIGPROCESSING IN OS 
