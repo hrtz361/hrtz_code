22cced613356b965798eb8266abf54e32541d2b0
* <0100useonly@gmail.com>
2026-01-08 01:23:43 +0700

    modified:   C/signal.c 	deleted:    LINUX_Lp_trinh_shell_1.pdf 	deleted:    README.md 	deleted:    python.pdf

--- C/signal.c
+++ C/signal.c
@@ -3,13 +3,15 @@
 #include<stdlib.h>
 // what is handler is it get input by stdin ? 
 void func(int err){
-	printf("get signal from insdec id:%d",err);
+	printf("get signal from insdec id:%d ",err);
+	printf("\nthe number?:%d\n",SIGINT); 
+	printf("the number?:%d\n",SIGQUIT); 
 	printf("the number?:%d\n",SIGKILL); 
-	printf("the number?:%d\n",SIGINT); 
 	printf("the number?:%d\n",SIGSEGV); 
 	printf("the number?:%d\n",SIGCHLD);
 	printf("the number?:%d\n",SIGPIPE);
-
+	printf("the number?:%d\n",SIGTERM);
+	printf("the number?:%d\n",SIGSTOP);
 	exit(1); 
 }
 
